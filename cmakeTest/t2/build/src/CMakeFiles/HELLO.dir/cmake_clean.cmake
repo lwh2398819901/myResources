e51ade6e9c4055ce86b2445cb7d463d6246c0149
lwh2398819901 <2398819901@qq.com>
2021-01-17 18:34:03 +0800

    增加资源文件

---
+++ cmakeTest/t2/build/src/CMakeFiles/HELLO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HELLO.dir/main.o"
+  "../bin/HELLO.pdb"
+  "../bin/HELLO"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HELLO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
